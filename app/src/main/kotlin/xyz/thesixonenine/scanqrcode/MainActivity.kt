9adb710c84a4bf0d2fe88dd2fe226ab7ef391eb7
Simple <huyang1994822@gmail.com>
2024-08-21 17:51:42 +0800

    refactor

--- app/src/main/kotlin/xyz/thesixonenine/scanqrcode/MainActivity.kt
+++ app/src/main/kotlin/xyz/thesixonenine/scanqrcode/MainActivity.kt
@@ -1,6 +1,7 @@
 package xyz.thesixonenine.scanqrcode
 
 import android.Manifest
+import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Intent
 import android.os.Bundle
@@ -27,7 +28,7 @@ class MainActivity : AppCompatActivity() {
         val integrator = IntentIntegrator(this)
         // 设置要扫描的条码类型, ONE_D_CODE_TYPES: 一维码, QR_CODE_TYPES: 二维码
         integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES)
-        integrator.setPrompt("扫描条形码")
+        integrator.setPrompt("请扫描二维码/条形码")
         // 使用默认的相机
         integrator.setCameraId(0)
         // 扫到码后播放提示音
@@ -39,12 +40,12 @@ class MainActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
         if (result != null) {
-            if (result.contents == null) {
-                Toast.makeText(this, "扫码取消！", Toast.LENGTH_LONG).show()
+            val contents = result.contents
+            if (contents == null) {
+                Toast.makeText(this, "扫码取消", Toast.LENGTH_LONG).show()
             } else {
-                textView!!.text = result.contents
-                Toast.makeText(this, "扫描成功，条码值: " + result.contents, Toast.LENGTH_LONG)
-                    .show()
+                textView!!.text = contents
+                Toast.makeText(this, "扫描内容: $contents", Toast.LENGTH_LONG).show()
             }
         } else {
             // This is important, otherwise the result will not be passed to the fragment
@@ -59,10 +60,9 @@ class MainActivity : AppCompatActivity() {
 
     fun onClickCopy(v: View?) {
         // 从API11开始android推荐使用android.content.ClipboardManager
-        // 为了兼容低版本我们这里使用旧版的android.text.ClipboardManager，虽然提示deprecated，但不影响使用。
+        // 为了兼容低版本我们这里使用旧版的android.text.ClipboardManager, 虽然提示deprecated, 但不影响使用
         val cm = getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
-        // 将文本内容放到系统剪贴板里。
-        cm.text = textView!!.text
-        Toast.makeText(this, "复制成功，可以发给朋友们了。", Toast.LENGTH_LONG).show()
+        cm.setPrimaryClip(ClipData.newPlainText("text", textView!!.text))
+        Toast.makeText(this, "已复制", Toast.LENGTH_LONG).show()
     }
 }
