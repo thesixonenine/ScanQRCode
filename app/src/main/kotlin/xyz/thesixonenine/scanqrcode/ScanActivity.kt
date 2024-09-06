c4ead3743f282b9ef8b0fa12b6cfe5c02f715184
Simple <huyang1994822@gmail.com>
2024-09-06 18:22:10 +0800

    hide status bar

--- app/src/main/kotlin/xyz/thesixonenine/scanqrcode/ScanActivity.kt
+++ app/src/main/kotlin/xyz/thesixonenine/scanqrcode/ScanActivity.kt
@@ -1,5 +1,8 @@
 package xyz.thesixonenine.scanqrcode
 
+import android.graphics.Color
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
 import com.journeyapps.barcodescanner.CaptureActivity
 import com.journeyapps.barcodescanner.DecoratedBarcodeView
 
@@ -7,7 +10,10 @@ import com.journeyapps.barcodescanner.DecoratedBarcodeView
 class ScanActivity : CaptureActivity() {
 
     override fun initializeContent(): DecoratedBarcodeView {
-        setContentView(R.layout.activity_scan);
+        setContentView(R.layout.activity_scan)
+        val controller = WindowCompat.getInsetsController(window, window.decorView)
+        controller.hide(WindowInsetsCompat.Type.statusBars())
+        window.statusBarColor = Color.TRANSPARENT
         return findViewById(R.id.zxing_barcode_scanner)
     }
 }
